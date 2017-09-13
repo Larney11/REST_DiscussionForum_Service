5136ff4ef5d53c8747fe5ce7a6018792df5ab37f
Lar <Lar@DESKTOP-5KMSFD7>
2017-09-13 14:32:52 +0100

    Add project files.

---
+++ REST_DiscussionForum_Service/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace REST_DiscussionForum_Service.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ REST_DiscussionForum_Service/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace REST_DiscussionForum_Service.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
